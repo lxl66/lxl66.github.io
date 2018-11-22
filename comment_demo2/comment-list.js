3948ba8d0dc0078ed888ae998708b5d4a2efd8df
Brother Li <42125945+lxl66@users.noreply.github.com>
2018-11-22 09:07:07 +0800

    Add files via upload

---
+++ comment_demo2/comment-list.js
@@ -0,0 +1,21 @@
+//创建组件
+var commentList = {
+    props: ['comments'],
+    template: `
+       <div class='clist'>
+           <comment v-for='c in comments'
+               v-bind:comment='c'>
+           </comment>
+       </div>
+    `
+};
+
+var commentItem = {
+    props: ['comment'],
+    template: `
+       <div class='comment'>
+           <span class='comment-author'>{{comment.author}}:</span>
+           <span class='comment-content'>{{comment.content}}</span>
+       </div>
+    `
+};
